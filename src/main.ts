2d6fb98d975cdf54cf33d573f05f4c5d67523970
zach-ma <alexma4cv@gmail.com>
2020-11-16 22:16:45 +0800

    add httpexception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, HttpException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
 import { BaseTransformInterceptor } from './shared/interceptors/base-transform.interceptor';
+import { HttpExceptionFilter } from './shared/interceptors/http-exception.filter';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalInterceptors(new BaseTransformInterceptor());
+  app.useGlobalInterceptors(new HttpExceptionFilter() as any);
 
   // swagger
   const options = new DocumentBuilder()
