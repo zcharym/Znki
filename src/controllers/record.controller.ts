d7b5a22b5d6d0fa25a13d9f99ed95693d2d6aa91
zach-ma <alexma4cv@gmail.com>
2020-11-16 22:42:10 +0800

    fix: "getmany error issue in @nestjsx/crud"

--- src/controllers/record.controller.ts
+++ src/controllers/record.controller.ts
@@ -18,33 +18,6 @@ import {
 })
 @ApiTags('Record')
 @Controller('record')
-export class RecordController implements CrudController<Record> {
-  constructor(private recordService: RecordService) {}
-  service: CrudService<Record>;
-  getManyBase?(
-    req: CrudRequest,
-  ): Promise<GetManyDefaultResponse<Record> | Record[]> {
-    throw new Error('Method not implemented.');
-  }
-  getOneBase?(req: CrudRequest): Promise<Record> {
-    throw new Error('Method not implemented.');
-  }
-  createOneBase?(req: CrudRequest, dto: Record): Promise<Record> {
-    throw new Error('Method not implemented.');
-  }
-  createManyBase?(
-    req: CrudRequest,
-    dto: CreateManyDto<Record>,
-  ): Promise<Record[]> {
-    throw new Error('Method not implemented.');
-  }
-  updateOneBase?(req: CrudRequest, dto: Record): Promise<Record> {
-    throw new Error('Method not implemented.');
-  }
-  replaceOneBase?(req: CrudRequest, dto: Record): Promise<Record> {
-    throw new Error('Method not implemented.');
-  }
-  deleteOneBase?(req: CrudRequest): Promise<void | Record> {
-    throw new Error('Method not implemented.');
-  }
+export class RecordController {
+  constructor(private service: RecordService) {}
 }
