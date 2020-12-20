83c75b4b583321844ff120743c62846b5a90cc2f
zach-ma <alexma4cv@gmail.com>
2020-12-20 16:37:00 +0800

    fix: db connection

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,19 +1,1 @@
-function deploy() {
-  if [ ! "$(docker ps -q -f name=znki)" ]; then
-  docker restart znki
-  if [ "$(docker ps -aq -f status=exited -f name=znki)" ]; then
-      # cleanup
-      echo "removing znki container"
-      docker rm znki
-  fi
-  # run your container
-  echo "running znki"
-  docker run \
-    -d --name znki -p 3000:3000 \
-    -e NODE_ENV=production \
-    -v /home/REMOTE_USER:/app node \
-    bash -c "cd /app;npm install && node main.js"
-  fi
-}
-
-deploy;
+docker_app_dir=/root/docker-appsfunction copy_modules() {  cd $docker_app_dir/tmp  npm install  cd -  rsync -a $docker_app_dir/tmp/node_modules $docker_app_dir/3-znki  cd -}function deploy() {  if [ ! "$(docker ps -q -f name=<znki>)" ]; then      if [ "$(docker ps -aq -f status=exited -f name=<name>)" ]; then          # cleanup          echo "===container already exists==="          docker restart znki      fi      # run your container      echo "===running znki==="      docker run \        -d --name znki -p 3000:3000 \        -e NODE_ENV=production \        -v /root/docker-apps/3-znki:/app node \        bash -c "cd /app;node main.js"  fi}copy_modules;deploy;
