b31e37448fbcac0664a4d35bebfcc0fe6f227095
Zchary-Ma <Zcharyma@gmail.com>
2021-10-14 16:09:44 +0800

    chore: add logger

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,13 +4,29 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
+	"os"
+	"time"
 )
 
 func Setup() {
 	var err error
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "[GORM]", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
 	dsn := "root:Admin@1234@tcp(127.0.0.1:3306)/znki?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		fmt.Println(err.Error())
 		log.Panic("Failed to connect to database")
